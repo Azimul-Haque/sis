75dcba3ab7f9cb8aa43b920bfe72625f7f09cf4f
A. H. M. Azimul Haque <orbachinujbuk@gmail.com>
2021-06-03 00:00:11 +0600

    ASDASD32424

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -438,15 +438,15 @@ class DashboardController extends Controller
 
         $expense->save();
 
-        // OneSignal::sendNotificationToUser(
-        //     "ব্যয় করেছেনঃ " . Auth::user()->name . ', খাতঃ ' . $category_data[1],
-        //     "a1050399-4f1b-4bd5-9304-47049552749c", 
-        //     $url = null, 
-        //     $data = null, // array("answer" => $charioteer->answer), // to send some variable
-        //     $buttons = null, 
-        //     $schedule = null,
-        //     $headings = $site_data[1] ."-এ ৳ " . bangla($request->amount) . " ব্যয় করা হয়েছে!"
-        // );
+        OneSignal::sendNotificationToUser(
+            "ব্যয় করেছেনঃ " . Auth::user()->name . ', খাতঃ ' . $category_data[1],
+            "a1050399-4f1b-4bd5-9304-47049552749c", 
+            $url = null, 
+            $data = null, // array("answer" => $charioteer->answer), // to send some variable
+            $buttons = null, 
+            $schedule = null,
+            $headings = $site_data[1] ."-এ ৳ " . bangla($request->amount) . " ব্যয় করা হয়েছে!"
+        );
         // OneSignal::sendNotificationToUser(
         //     "Test",
         //     "a1050399-4f1b-4bd5-9304-47049552749c", 
