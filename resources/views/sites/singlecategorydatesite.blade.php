82e3c644f28778163bd5aeaf76438a9af2c0f72b
orbachinujbuk <orbachinujbuk@gmail.com>
2021-11-15 16:49:35 +0600

    SADSD

---
+++ resources/views/sites/singlecategorydatesite.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.app')
+@section('title') ড্যাশবোর্ড | খাতের তারিখভিত্তিক খরচের হিসাব @endsection
+
+@section('third_party_stylesheets')
+
+@endsection
+
+@section('content')
+	@section('page-header') {{ $category->name }} - {{ bangla(date('F d, Y', strtotime($selecteddate))) }} @endsection
+    <div class="container-fluid">
+		<div class="card">
+          <div class="card-header">
+            <h3 class="card-title">{{ $category->name }} - {{ bangla(date('F d, Y', strtotime($selecteddate))) }}</h3>
+
+            <div class="card-tools">
+        	    {{-- <button type="button" class="btn btn-success btn-sm"  data-toggle="modal" data-target="#addCategoryModal">
+          			<i class="fas fa-plus-square"></i> খাত যোগ
+          		</button> --}}
+              {{-- <ul class="pagination pagination-sm float-right">
+                <li class="page-item"><a class="page-link" href="#">«</a></li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item"><a class="page-link" href="#">»</a></li>
+              </ul> --}}
+            </div>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body p-0">
+            <table class="table">
+              <tbody>
+                {{-- <tr>
+                  <td>1.</td>
+                  <td>Update software</td>
+                  <td>
+                    <div class="progress progress-xs">
+                      <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                    </div>
+                  </td>
+                  <td><span class="badge bg-danger">55%</span></td>
+                </tr> --}}
+                @foreach($expenses as $expense)
+                	<tr>
+                    <td style="line-height: 1;">
+                      <span class="badge bg-primary">ব্যয়ঃ ৳ {{ $expense->amount }}</span>
+                      <span class="badge bg-success">খাতঃ {{ $expense->category->name }}</span>
+                      <span class="badge bg-info">পরিমাণঃ {{ $expense->qty }}</span><br/>
+                      <small>
+                          <span class="text-black-50">ব্যয় করেছেনঃ</span> {{ $expense->user->name }},
+                          <span class="badge bg-warning"><big>সাইটঃ {{ $expense->site->name }}</big></span><br/>
+                          <span>{{ date('F d, Y h:i A', strtotime($expense->created_at)) }}</span>, 
+                          <span class="text-black-50">বিবরণঃ </span> {{ $expense->description }}
+                      </small> 
+                    </td>
+                	</tr>
+                @endforeach
+              </tbody>
+            </table>
+          </div>
+          <!-- /.card-body -->
+        </div>
+    </div>
+
+@endsection
+
+@section('third_party_scripts')
+
+@endsection
